37f4acf9d5b0b9e023312d2b169f11c04e763839
Ivan Redreev <redreev.ivan@gmail.com>
2021-01-18 22:06:04 +0500

    Solution

--- Testovoe/ViewController.swift
+++ Testovoe/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let str = "\nab\n".outputRange(position: 1)
+        print(str)
+        print(str.firstHalf)
+        print(str.secondHalf)
     }
-
-
+    
 }
+
+extension String {
+    func outputRange(position: Int) -> (firstHalf: String, secondHalf: String) {
+        let index = self.index(self.startIndex, offsetBy: position)
+        let symbol = self[index]
+        
+        if symbol.isNewline || symbol.isWhitespace {
+            return(firstHalf: "", secondHalf: "")
+        }
+        
+        let substring1 = self[..<index].split(separator: "\n").last
+        let substring2 = self[index...].split(separator: "\n").first
+        
+        let firstHalf = String(substring1 ?? "")
+        let secondHalf = String(substring2 ?? "")
+        
+        return(firstHalf: firstHalf, secondHalf: secondHalf)
+    }
+}
+
 
